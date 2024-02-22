31c7613f7c6364ca1b0652405133deea85d3076f
hanzalah5 <hanzalah435@gmail.com>
2024-02-22 22:19:02 +0500

    calculator flask implemented

--- app.py
+++ app.py
@@ -1,36 +1,30 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Define routes
-@app.route('/')
-def index():
-    return render_template('index.html')
+# Define routes for different arithmetic operations
+@app.route('/calc/add/<int:num1>/<int:num2>')
+def add(num1, num2):
+    result = num1 + num2
+    return str(result)
 
-@app.route('/calculate', methods=['POST'])
-def calculate():
-    if request.method == 'POST':
-        num1 = float(request.form['num1'])
-        num2 = float(request.form['num2'])
-        operation = request.form['operation']
+@app.route('/calc/subtract/<int:num1>/<int:num2>')
+def subtract(num1, num2):
+    result = num1 - num2
+    return str(result)
 
-        if operation == 'add':
-            result = num1 + num2
-            operation_symbol = '+'
-        elif operation == 'subtract':
-            result = num1 - num2
-            operation_symbol = '-'
-        elif operation == 'multiply':
-            result = num1 * num2
-            operation_symbol = '*'
-        elif operation == 'divide':
-            if num2 != 0:
-                result = num1 / num2
-                operation_symbol = '/'
-            else:
-                return "Error: Cannot divide by zero!"
+@app.route('/calc/multiply/<int:num1>/<int:num2>')
+def multiply(num1, num2):
+    result = num1 * num2
+    return str(result)
 
-        return render_template('result.html', num1=num1, num2=num2, operation_symbol=operation_symbol, result=result)
+@app.route('/calc/divide/<int:num1>/<int:num2>')
+def divide(num1, num2):
+    if num2 != 0:
+        result = num1 / num2
+        return str(result)
+    else:
+        return "Error: Cannot divide by zero!"
 
 if __name__ == '__main__':
     app.run(debug=True)
